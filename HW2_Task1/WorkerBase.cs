0ae011e26788669bb30ba653ef21a2142d061362
tasmtony <tasmtony@yandex.ru>
2017-03-23 17:37:23 +0900

    Создал проект для первого задания

---
+++ HW2_Task1/WorkerBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2_Task1
+{
+    abstract class WorkerBase
+    {
+        private string Fname;
+        private string Lname;
+        private int Salary;
+
+        public WorkerBase(string _Fname, string _Lname, int _salary)
+        {
+            Fname = _Fname;
+            Lname = _Lname;
+            Salary = _salary;
+        }
+
+
+    }
+}
